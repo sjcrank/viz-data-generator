827344a296019411ae13503fec2de75cb7108b61
Samuel Crank <sjcrank@gmail.com>
2016-09-09 23:11:12 -0400

    add styles

--- src/header.js
+++ src/header.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import StyleGuide from './styleGuide';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div>
-                <span>Viz Data Generator</span>
-                <a href='https://github.com/sjcrank/viz-data-generator'>source code</a>
+            <div className={css(styles.header)}>
+                <div className={css(styles.headerContent)}>
+                    <span className={css(styles.title)}>Viz Data Generator</span>
+                    <a className={css(styles.link)} href='https://github.com/sjcrank/viz-data-generator'>source code</a>
+                </div>
             </div>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: StyleGuide.colorBlue,
+    },
+    headerContent: {
+        maxWidth: '1100px',
+        margin: '0 auto',
+        padding: '9px 12px',
+    },
+    title: {
+        color: '#ffffff',
+        fontSize: StyleGuide.fontSize2,
+        lineHeight: StyleGuide.lineHeight2,
+        letterSpacing: '1px',
+    },
+    link: {
+        color: 'rgba(255,255,255,0.7)',
+        fontSize: StyleGuide.fontSize3,
+        lineHeight: StyleGuide.lineHeight2,
+        fontWeight: StyleGuide.weight4,
+        letterSpacing: '1px',
+        float: 'right',
+        textDecoration: 'none',
+        ':hover': {
+            textDecoration: 'underline',
+            color: 'rgba(255,255,255,0.8)',
+        }
+    },
+});
---
+++ src/styleGuide.js
@@ -0,0 +1,21 @@
+export default {
+    // colors
+    colorBlue: '#00b5ea',
+    // font sizes
+    fontSize1: '20px',
+    fontSize2: '16px',
+    fontSize3: '14px',
+    fontSize4: '13px',
+    fontSize5: '12px',
+    // line heights
+    lineHeight1: '30px',
+    lineHeight2: '24px',
+    lineHeight3: '20px',
+    lineHeight4: '18px',
+    lineHeight5: '16px',
+    // font weights
+    weight1: '900',
+    weight2: '700',
+    weight3: '400',
+    weight4: '300',
+}
